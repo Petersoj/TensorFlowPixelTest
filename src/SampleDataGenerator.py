ebf299d414a3af589a7fb39a1b87c9f11672ac97
Yrish <Yrish3@gmail.com>
2019-02-08 18:07:27 -0700

    Better sample generation

--- src/SampleDataGenerator.py
+++ src/SampleDataGenerator.py
@@ -27,13 +27,22 @@ class SampleDataGenerator:
         label = SampleDataGenerator.__labels__[int(random.random() * len(SampleDataGenerator.__labels__))]
         base = random.random() * 0.75 + 0.25
         if label == SampleDataGenerator.Constants.Solid:
-            return ([base, SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1)], SampleDataGenerator.Constants.Solid)
+            return [[base, SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1)], SampleDataGenerator.Constants.Solid]
         elif label == SampleDataGenerator.Constants.Diagonal:
-            return ([base, SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1)], SampleDataGenerator.Constants.Diagonal)
+            tmp = [[base, SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1)], SampleDataGenerator.Constants.Diagonal]
+            if random.random() >= 0.5:
+                tmp[0] = [*tmp[0][2:], *tmp[0][0:2]]
+            return tmp
         elif label == SampleDataGenerator.Constants.Horizontal:
-            return ([base, SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1)], SampleDataGenerator.Constants.Horizontal)
+            tmp = [[base, SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1)], SampleDataGenerator.Constants.Horizontal]
+            if random.random() >= 0.5:
+                tmp[0] = [*tmp[0][2:], *tmp[0][0:2]]
+            return tmp
         elif label == SampleDataGenerator.Constants.Vertical:
-            return ([base, SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1)], SampleDataGenerator.Constants.Vertical)
+            tmp = [[base, SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 10 - 0.05) * base,0,1), SampleDataGenerator.capture(base + (random.random() / 3 - 0.5) * base,0,1)], SampleDataGenerator.Constants.Vertical]
+            if random.random() >= 0.5:
+                tmp[0] = [*tmp[0][2:], *tmp[0][0:2]]
+            return tmp
         else:
             tmp = ([base, SampleDataGenerator.capture(base + (random.random() / 20 - 0.05) * base, 0, 1),
                      SampleDataGenerator.capture(base + (random.random() / 20 - 0.05) * base, 0, 1),
@@ -46,6 +55,7 @@ class SampleDataGenerator:
                 tmp[0][randindex] += 0.45
             tmp[0][randindex] = SampleDataGenerator.capture(tmp[0][randindex], 0, 1)
             return tmp
+
     @classmethod
     def generateSamples(cls, amount, seed=None):
         if not seed == None:
