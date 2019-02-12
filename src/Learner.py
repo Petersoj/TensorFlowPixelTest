66720bc849e74998832bf1cd713ab1e3476e6087
Jacob Peterson <git@jacobpeterson.net>
2019-02-12 16:17:28 -0700

    Added grayscale display and manual testing

--- src/Learner.py
+++ src/Learner.py
@@ -8,7 +8,7 @@ from SampleDataGenerator import SampleDataGenerator
 
 print(dir(SampleDataGenerator))
 
-SAMPLE_SIZE = 50000
+SAMPLE_SIZE = 100_000
 SAMPLES = []
 SAMPLE_LABELS = []
 
@@ -22,8 +22,9 @@ SAMPLE_LABELS = np.array(SAMPLE_LABELS)
 print(SAMPLES)
 
 model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu),
     keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Dense(5, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer='adam',
@@ -32,9 +33,14 @@ model.compile(optimizer='adam',
 
 model.fit(SAMPLES, SAMPLE_LABELS, epochs=5)
 
+model.save_weights("./weights")
+
+# model.load_weights("./weights")
+
 test_samples = []
 test_labels = []
-for sample in SampleDataGenerator.generateSamples(5000):
+
+for sample in SampleDataGenerator.generateSamples(500):
     test_samples.append(sample[0])
     test_labels.append(sample[1])
 
@@ -43,3 +49,17 @@ test_labels = np.array(test_labels)
 
 test_loss, test_acc = model.evaluate(test_samples, test_labels)
 print('Test accuracy:', test_acc)
+
+print(test_samples[10], end=" ")
+print(test_labels[10])
+print(test_samples.shape)
+
+to_predict = np.array([test_samples[10]])
+print(to_predict.shape)
+
+print(model.predict(to_predict) * 10)
+
+
+plt.imshow(np.array([test_samples[10][0:2] * 255, test_samples[10][2:] * 255]), cmap="gray")
+plt.show()
+
